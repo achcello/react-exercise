c3ed001171fa5e799012c35b06a91f271e980100
Aryn Harmon <achcello@gmail.com>
2018-09-17 22:10:50 -0500

    add modified list

--- src/components/Instructions.component.js
+++ src/components/Instructions.component.js
@@ -12,12 +12,34 @@ class Instructions extends Component {
             src="https://uiuc.hack4impact.org/img/colored-logo.png"
             alt="h4i logo"
           />
+          {this.props.things.map((thing, index) => (
+            <ul key={index}>
+              {thing.length >= 3 && (
+                <li>
+                  {this.props.things.indexOf(thing) % 2 == 1
+                    ? thing.toUpperCase()
+                    : thing}
+                </li>
+              )}
+            </ul>
+          ))}
         </div>
       )
     } else {
       return (
         <div className="instructions">
           Follow the instructions on the README to get started!
+          {this.props.items.map((thing, index) => (
+            <ul key={index}>
+              {thing.length >= 3 && (
+                <li>
+                  {this.props.things.indexOf(thing) % 2 == 1
+                    ? thing.toUpperCase()
+                    : thing}
+                </li>
+              )}
+            </ul>
+          ))}
         </div>
       )
     }
